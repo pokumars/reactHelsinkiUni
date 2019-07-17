b5c6bda7f0fda00a764cb8c2a3085cd1edaffd6d
Oheneba <ohenebap@metropolia.fi>
2019-07-17 14:00:38 +0300

    Ex 1.4 done

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -2,12 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const Header = (props) => {
+    console.log(props);
     return(
         <h1>{props.course}</h1>
     )    
 }
 
 const Part = (props) => {
+
     return (
         <div>
             <p>{props.part} {props.exercises}</p>
@@ -16,50 +18,48 @@ const Part = (props) => {
 }
 
 const Content = (props) => {
-return (
+    console.log(props.parts[0]);
+return (    
     <div>
-        <Part part={props.part1} exercises={props.exercises1} />
-        <Part part={props.part2} exercises={props.exercises2} />
-        <Part part={props.part3} exercises={props.exercises3} />
+        <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
+        <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
+        <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
+        
     </div>
-)
+);
 }
 
 const Total = (props) => {
+    console.log(props);
     return (
         <div>
-            <p>Number of exercises {props.total}</p>
+            <p>Number of exercises {props.total[0].exercises +props.total[1].exercises + props.total[2].exercises}</p>
         </div>
     )
 }
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-      name: 'Fundamentals of React',
-      exercises : 10
-  }
-  const part2 = {
+  const partsArr = [
+    {
+        name: 'Fundamentals of React',
+        exercises : 10
+    },
+    {
         name: 'Using props to pass data',
         exercises : 7
-    }
-    const part3 = {
+    }, 
+    {
         name: 'State of a component',
         exercises : 14
     }
-  /*const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14*/
-
+];
+   
   return (
     <div>
         <Header course={course} />
-        < Content part1={part1.name} part2={part2.name} part3={part3.name} 
-        exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises} />
-        <Total total={part1.exercises + part2.exercises + part3.exercises}/>
+        <Content parts={partsArr}/>
+        <Total total={partsArr}/>
     </div>
   )
 }
